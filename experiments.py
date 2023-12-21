8c3b2f6b2736debe9d3444ce61cfe63a1186b940
Milagros Aylén Jacinto <mili.aylen.j@gmail.com>
2023-12-21 17:55:27 -0300

    Arregla pequeños detalles

--- experiments.py
+++ experiments.py
@@ -7,14 +7,13 @@ from keras.layers import Dense
 from keras.utils import np_utils
 from sklearn.datasets import load_wine, load_iris, load_breast_cancer
 
-# Pasar el y con np_utils.to_categorical(copy.deepcopy(Y_train),num_classes=classes)
-
-def build_model(hidden_layer_sizes,input_dim, classes, X , Y, persentage_test ,loss ):
+def build_model(hidden_layer_sizes ,input_dim ,X ,Y ,persentage_test ,loss):
 
     end_idx=len(X)*1-persentage_test
     
     X_train = X[:end_idx]
     Y_train = Y[:end_idx]
+    y_train = np_utils.to_categorical(copy.deepcopy(Y_train),num_classes=CANT_CLASES)
 
     model = Sequential()
 
@@ -28,7 +27,7 @@ def build_model(hidden_layer_sizes,input_dim, classes, X , Y, persentage_test ,l
 
     model.compile(loss=loss,optimizer='adam',metrics=['accuracy'])
 
-    model.fit(X_train, Y_train, batch_size=32,epochs=200,verbose=1)
+    model.fit(X_train, y_train, batch_size=32, epochs=200, verbose=1)
     
     return model
 
@@ -37,10 +36,10 @@ if (conf.DATASET == "iris"):
     CANT_CLASES = len(iris.target_names)
     INPUT_DIM = len(iris.feature_names)
     ATTRIBUTES = iris.feature_names
-    LOSS_FUNCTION = ''
+    LOSS_FUNCTION = 'categorical_crossentropy'
     X, Y = shuffle(iris.data , iris.target)
-    
-    HIDDEN_LAYER_SIZES= [5,CANT_CLASES]
+
+    HIDDEN_LAYER_SIZES = [5,CANT_CLASES]
     
     
 elif (conf.DATASET == "wbc"):
@@ -48,7 +47,7 @@ elif (conf.DATASET == "wbc"):
     CANT_CLASES = len(wbc.target_names)
     INPUT_DIM = len(wbc.feature_names)
     ATTRIBUTES = wbc.feature_names
-    LOSS_FUNCTION = ''
+    LOSS_FUNCTION = 'binary_crossentropy'
     X, Y = shuffle(wbc.data , wbc.target)
     
     HIDDEN_LAYER_SIZES = [10,CANT_CLASES]
@@ -59,13 +58,13 @@ else:
     CANT_CLASES = len(wine.target_names)
     INPUT_DIM = len(wine.feature_names)
     ATTRIBUTES = wine.feature_names
-    LOSS_FUNCTION = ''
+    LOSS_FUNCTION = 'categorical_crossentropy'
     X, Y = shuffle(wine.data , wine.target)
     
-    HIDDEN_LAYER_SIZES = [10, CANT_CLASES]
+    HIDDEN_LAYER_SIZES = [10,CANT_CLASES]
+
+KERAS_MODEL = build_model(HIDDEN_LAYER_SIZES ,INPUT_DIM ,X ,Y ,conf.TEST_PERCENT,LOSS_FUNCTION)
 
 
-KERAS_MODEL = build_model(HIDDEN_LAYER_SIZES,INPUT_DIM,CANT_CLASES,X,Y, LOSS_FUNCTION)
-
-forxren = FORxREN().extract_rules(KERAS_MODEL, X, Y, INPUT_DIM, conf.FIRST_LAYER_SIZE, conf.EXECUTION_MODE, 
-                                 conf.TEST_PERCENT, conf.MAX_FIDELITY_LOSS, ATTRIBUTES)
+forxren = FORxREN().extract_rules(KERAS_MODEL ,X ,Y ,INPUT_DIM ,conf.FIRST_LAYER_SIZE ,
+                                    conf.EXECUTION_MODE, conf.TEST_PERCENT, conf.MAX_FIDELITY_LOSS, ATTRIBUTES)
