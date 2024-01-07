72ba477e0160e51077aadd536604f32e2ade9607
Martin Moschettoni <moschettonimartin@hotmail.com>
2024-01-07 12:25:25 -0300

    Agrega archivos de resultados

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 #Configuration
-DATASET = "wine" #Default iris - options: "iris", "wbc", "wine"
+DATASET = "iris" #Default iris - options: "iris", "wbc", "wine"
 EXECUTION_MODE = 1 #Default 1 (complete algorithm), options: 1, 2 (No Updation of rules), 3 (Initial rules only, no pruning nor updation)
 TEST_PERCENT = 0.2 #Default 0.2 - $ of data used for testing, 0.2 = 20%
 MAX_FIDELITY_LOSS = 0.05
